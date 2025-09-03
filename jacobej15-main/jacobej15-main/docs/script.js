0f8cb6a65a79bc77638624c58f25a3e86d814f27
jacobej15 <118198511+jacobej15@users.noreply.github.com>
2025-09-03 10:47:02 -0500

    Add files via upload

---
+++ jacobej15-main/jacobej15-main/docs/script.js
@@ -0,0 +1,19 @@
+document 
+.addEventListener("close", doThisFunction);
+("DOMContentLoaded,"()=> {
+console.log("Document successfully loaded, baby")
+//Window.alert("hi");
+
+document.getElementById
+    }
+);
+alert("hi");
+function btnClickyAction(){
+
+    console.log('in btnClicky event');
+    document.getElementById('btnClicky')
+    .forEach(el => {el.style.transform = 'rotate(15deg)'})
+    console.log('after rotation')
+}
+
+const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
